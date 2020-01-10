42353247abc625d75053c74bae42d32917b3a83c
Ian Oliver M. Del Rosario <IanOlver.DelRosario@summitmedia.com.ph>
2020-01-10 10:30:46 +0800

    custom message for rules

--- src/Base/BaseFormRequest.php
+++ src/Base/BaseFormRequest.php
@@ -7,6 +7,8 @@ use Vellum\Contracts\Resource;
 
 class BaseFormRequest extends FormRequest
 {
+    private $messages = [];
+    
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -25,7 +27,23 @@ class BaseFormRequest extends FormRequest
     public function rules(Resource $resource)
     {
         $fields = $resource->getAttributes();
-        
+
+        array_key_exists('messages', $fields) ? $this->setMessages($fields) : '';
+
         return array_key_exists('rules', $fields) ? $fields['rules'] : [];
+    } 
+
+    public function messages()
+    {
+        return $this->messages;
+    }
+
+    private function setMessages($fields)
+    {
+        collect($fields['messages'])->each(function ($rules, $name) {
+            collect($rules)->each(function ($value, $key) use ($name) {
+                $this->messages[$name . '.' . $key] = $value;
+            });
+        });
     }
 }
