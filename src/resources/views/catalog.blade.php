6942f8adcb25dc602f251cfddf018c27694111f1
Ian Oliver M. Del Rosario <IanOlver.DelRosario@summitmedia.com.ph>
2020-01-17 17:17:34 +0800

    delete now working

--- src/resources/views/catalog.blade.php
+++ src/resources/views/catalog.blade.php
@@ -4,46 +4,73 @@
 
 @push('css')
 <style>
-    #filter:not(checked) ~ .filter-menu {
+    #filter:not(checked)~.filter-menu {
         display: none;
     }
-    #filter:checked ~ .filter-menu {
+
+    #filter:checked~.filter-menu {
         display: inline-block;
     }
-
 </style>
 @endpush
 
 @section('content')
 
-    <div class="">
+<div class="">
 
-        <h1 class="text-4xl font-bold mb-5 mt-10">{{ $details['title'] }}</h1>
+    <h1 class="text-4xl font-bold mb-5 mt-10">{{ $details['title'] }}</h1>
 
-        <form action="">
+    <form action="">
 
-	        <div class="flex mb-10">
+        <div class="flex mb-10">
 
-	            <div class="w-3/4">
-	                @include('vellum::search')
-	            </div>
+            <div class="w-3/4">
+                @include('vellum::search')
+            </div>
 
-	            @can('create', $details['model'])
+            @can('create', $details['model'])
 
-	            <div class="w-1/4 text-right">
+            <div class="w-1/4 text-right">
 
-            		@button(['action'=>'create', 'icon'=>'plus','color'=>'blue','label'=>'Create new'])
+                @button(['action'=>'create', 'icon'=>'plus','color'=>'blue','label'=>'Create new'])
 
-	            </div>
+            </div>
 
-	            @endcan
+            @endcan
 
-	        </div>
+        </div>
 
-	        @include('vellum::table', ['collections' => $collections, 'attributes' => $attributes])
+        @include('vellum::table', ['collections' => $collections, 'attributes' => $attributes])
 
-        </form>
+    </form>
 
-    </div>
-
+</div> 
 @endsection
+
+
+@push('scripts')
+<script>
+    $(function() {
+        $('.btn-delete').click(function(e) {
+
+
+
+            $.ajax({
+                url: $(this).attr('href'),
+                type: 'POST',
+                data: {
+                    _method: "delete",
+                    _token: $('meta[name="csrf-token"]').attr('content')
+                },
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(status) {
+                    console.log(status);
+                }
+            });
+            e.preventDefault();
+        });
+    });
+</script>
+@endpush
