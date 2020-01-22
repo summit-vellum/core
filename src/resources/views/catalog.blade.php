3b315d77b174cd1f81be6972424293e6a23d6e8e
Ian Oliver M. Del Rosario <IanOlver.DelRosario@summitmedia.com.ph>
2020-01-22 16:06:33 +0800

    updated path

--- src/resources/views/catalog.blade.php
+++ src/resources/views/catalog.blade.php
@@ -48,5 +48,5 @@
 @endsection
 
 @push('scripts')
-<script src="{{asset('js/custom.js')}}"></script>
+<script src="{{asset('vendor/vellum/js/custom.js')}}"></script>
 @endpush
--- src/resources/views/form.blade.php
+++ src/resources/views/form.blade.php
@@ -73,7 +73,7 @@
 
 @endsection
 @push('scripts') 
-<script src="{{asset('js/custom.js')}}"></script>
+<script src="{{asset('vendor/vellum/js/custom.js')}}"></script>
 @endpush
 @form
     @push('scripts') 
