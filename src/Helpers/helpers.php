d44a29361c063f44675c5e1ed0d8be342e27f7e1
Gien Mangaliman <Gien.Mangaliman@summitmedia.com.ph>
2020-02-10 15:39:56 +0800

    added server side js and css helper; deleted select js and css

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -42,7 +42,8 @@ function arrayToHtmlAttributes(array $attributes = array())
     return $attributeString;
 }
 
-function check_cross_auth() {
+function check_cross_auth()
+{
 
     if (auth()->user()) {
         $currentToken = auth()->user()->cross_token;
@@ -54,4 +55,44 @@ function check_cross_auth() {
     }
 
     return false;
+}
+
+function addScript($section, $url, $attributes = array(), $secure = null)
+{
+	$scriptKey = sha1($url);
+
+    $GLOBALS['scriptHelper'][$section]['script'][$scriptKey] = array(
+        'url' => $url,
+        'attributes' => $attributes,
+        'secure' => $secure,
+    );
+}
+
+function addStyle($section, $url, $attributes = array(), $secure = null)
+{
+	$scriptKey = sha1($url);
+
+    $GLOBALS['cssHelper'][$section]['style'][$scriptKey] = array(
+        'url' => $url,
+        'attributes' => $attributes,
+        'secure' => $secure,
+    );
+}
+
+function script($section)
+{
+	 if (isset($GLOBALS['scriptHelper'][$section]['script'])) {
+        foreach ($GLOBALS['scriptHelper'][$section]['script'] as $script) {
+            echo Html::script($script['url'], $script['attributes'], $script['secure']);
+        }
+    }
+}
+
+function style($section)
+{
+	if (isset($GLOBALS['cssHelper'][$section]['style'])) {
+		 foreach ($GLOBALS['cssHelper'][$section]['style'] as $style) {
+            echo Html::style($style['url'], $style['attributes'], $style['secure']);
+        }
+	}
 }
