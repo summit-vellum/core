8f74e2bafea08f1eb72fdf9e639c1d212797da3f
Gien Mangaliman <Gien.Mangaliman@summitmedia.com.ph>
2020-02-11 14:44:50 +0800

    fix for excess characters in catalog blade

--- src/resources/views/catalog.blade.php
+++ src/resources/views/catalog.blade.php
@@ -17,9 +17,9 @@
 @section('content')
 	@dialog(['id' => 'deleteResourceDialog'])
 
-	@include(template('maintenance'))
-    @include(template('search'))
-    @include(template('table', ['collections' => $collections, 'attributes' => $attributes]))
+	@include(template('maintenance')
+    @include(template('search')
+    @include(template('table', ['collections' => $collections, 'attributes' => $attributes])
 
     @section('actions')
     	@button(['action'=>'create', 'color'=>'blue','label'=>'+ New'])
