aa57ba2534b9b25a211d59a59dfb23ea91019e38
Gien Mangaliman <Gien.Mangaliman@summitmedia.com.ph>
2020-02-26 10:09:48 +0800

    core filter adjustment

--- src/resources/views/filter.blade.php
+++ src/resources/views/filter.blade.php
@@ -1,39 +1,39 @@
-@if(isset($filters) && !empty($filters))
-
 <div class="col-md-8 text-right">
-	<strong>result count here</strong>
+	<strong>{{ $collections->total() }} results</strong>
 
-	<div class="btn-group ml-2 text-left">
-		<button type="button" class="btn btn-secondary dropdown-toggle btn-flat btn-filter">
-	        @icon(['icon' => 'arrow-left'])
-	        Filter
-	   	</button>
-	   	<div class="dropdown dropdown-filter hide">
-	   		<div class="clearfix mb-2">
-	            <span class="btn btn-secondary dropdown-toggle btn-flat pull-right mb-2 btn-sm btn-cancel-filter" data-toggle="dropdown">
-					@icon(['icon' => 'arrow-right'])
-					Filter
-	            </span>
-	        </div>
-	        @foreach($filters as $filter => $values)
-	        <div class="btn-group btn-group-sm btn-block mb-2">
-	        	<select class="selectpicker form-control" name="{{ $filter }}">
-	        		<option value="0">-- Select {{ $filter }} --</option>
-	                @foreach($values as $id=>$value)
-	                    <option value="{{ $id }}"
-	                    @if(request($filter) == $id)
-	                    selected
-	                    @endif
-	                    >{{ $value }}</option>
-	                @endforeach
-	        	</select>
-	        </div>
-	        @endforeach
-	   	</div>
-	</div>
+	@if(isset($filters) && !empty($filters))
+		<div class="btn-group ml-2 text-left">
+			<button type="button" class="btn btn-secondary dropdown-toggle btn-flat btn-filter">
+		        @icon(['icon' => 'arrow-left'])
+		        Filter
+		   	</button>
+		   	<div class="dropdown dropdown-filter hide">
+		   		<div class="clearfix mb-2">
+		            <span class="btn btn-secondary dropdown-toggle btn-flat pull-right mb-2 btn-sm btn-cancel-filter" data-toggle="dropdown">
+						@icon(['icon' => 'arrow-right'])
+						Filter
+		            </span>
+		        </div>
+		        @foreach($filters as $filter => $values)
+		        <div class="btn-group btn-group-sm btn-block mb-2">
+		        	<select class="selectpicker form-control" name="{{ $filter }}">
+		        		<option value="0">-- Select {{ $filter }} --</option>
+		                @foreach($values as $id=>$value)
+		                    <option value="{{ $id }}"
+		                    @if(request($filter) == $id)
+		                    selected
+		                    @endif
+		                    >{{ $value }}</option>
+		                @endforeach
+		        	</select>
+		        </div>
+		        @endforeach
+		   	</div>
+		</div>
+	@endif
 </div>
 
-@endif
+
 
 
 @push('scripts')
